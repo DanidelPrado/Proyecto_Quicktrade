fc5bb9a2113676733a62b9c9ff73623c9256163e
DESKTOP-U7V5H6P\usuario <snait7@gmail.com>
2020-12-13 13:59:30 +0100

    subo ae7 sin acabar

---
+++ myFirstApp/src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  tecnologia : string = "Tecnologia";
+  hogar : string = "Hogar";
+  inmobiliaria : string = "Inmobiliaria";
+  motor : string = "Motor";
+  opcion : string = ""; 
+  ocultar_tecnologia: boolean = true;
+  ocultar_inmobiliaria: boolean = true;
+  ocultar_motor: boolean = true;
+  constructor() {}
+
+
+  cambiar_Oculto() : void {
+    if(this.opcion == this.tecnologia){
+      this.ocultar_tecnologia = false;
+      this.ocultar_inmobiliaria = true;
+      this.ocultar_motor = true;
+    } else if (this.opcion == this.inmobiliaria){
+      this.ocultar_tecnologia = true;
+      this.ocultar_inmobiliaria = false;
+      this.ocultar_motor = true;
+    } else if (this.opcion == this.motor){
+      this.ocultar_tecnologia = true;
+      this.ocultar_inmobiliaria = true;
+      this.ocultar_motor = false;
+    } else if (this.opcion == this.hogar){
+      this.ocultar_tecnologia = true;
+      this.ocultar_inmobiliaria = true;
+      this.ocultar_motor = true;
+    }
+  }
+}
