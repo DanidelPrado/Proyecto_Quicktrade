ccd7edd2f9c8bc2751698401251551ba88f2ffa3
DESKTOP-U7V5H6P\usuario <snait7@gmail.com>
2020-12-22 11:37:03 +0100

    subo ae8 (elegir categoria para ver los productos)

--- myFirstApp/src/app/home/home.page.ts
+++ myFirstApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProducto, IMotor, IInmobiliaria, ITecnologia } from '../interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +18,39 @@ export class HomePage {
   ocultar_inmobiliaria: boolean = true;
   ocultar_motor: boolean = true;
   type: string = "number";
-  constructor() {}
+  nombre: string;
+  descripcion : string;
+  categoria : string;
+  precio : number;
+  tipo_vehiculo : string;
+  opcion2 : string = "";
+  Km : number;
+  fabricacion : number;
+  opcion3: string = "";
+  estado_producto : string;
+  metros_cuadrados: number;
+  num_banyos: number;
+  num_habitaciones: number;
+  localidad: string;
+  productos: (IProducto | IMotor | IInmobiliaria | ITecnologia)[] = [
+    {
+      "id" : 1,
+      "nombre" : "Iphone 8",
+      "descripcion" : "Móvil iphone 8 de segunda mano de la marca Apple.",
+      "categoria" : "Tecnología",
+      "precio" : 350.,
+      "tipo_vehiculo": null,
+      "Km" : null,
+      "fabricacion": null,
+      "metros_cuadrados": null,
+      "num_banyos": null,
+      "num_habitaciones": null,
+      "localidad": null,
+      "estado_producto": "Usado"
+    }
+  ];
+
+  constructor(private _toastCtrl : ToastController) {}
 
 
   cambiar_Oculto() : void {
@@ -37,5 +71,67 @@ export class HomePage {
       this.ocultar_inmobiliaria = true;
       this.ocultar_motor = true;
     }
+  }
+
+  insertarMotor(){
+
+    let motor: IMotor={"id": this.productos.length+1, 
+                            "nombre": this.nombre,
+                            "descripcion": this.descripcion,
+                            "categoria": this.opcion,
+                            "precio": this.precio,
+                            "tipo_vehiculo": this.opcion2,
+                            "Km" : this.Km,
+                            "fabricacion": this.fabricacion
+                                                    };
+
+        this.productos.push(motor);
+
+        this.presentToast();   
+
+  }
+
+  insertarTecno(){
+
+    let tecno: ITecnologia={"id": this.productos.length+1, 
+                            "nombre": this.nombre,
+                            "descripcion": this.descripcion,
+                            "categoria": this.opcion,
+                            "precio": this.precio,
+                            "estado_producto": this.opcion3
+                                                    };
+
+        this.productos.push(tecno);
+
+        this.presentToast();   
+
+  }
+
+  insertar(){
+
+    let inmob: IInmobiliaria={"id": this.productos.length+1, 
+                            "nombre": this.nombre,
+                            "descripcion": this.descripcion,
+                            "categoria": this.opcion,
+                            "precio": this.precio,
+                            "metros_cuadrados": this.metros_cuadrados,
+                            "num_banyos": this.num_banyos,
+                            "num_habitaciones": this.num_habitaciones,
+                            "localidad": this.localidad
+                                                    };
+
+        this.productos.push(inmob);
+
+        this.presentToast();   
+
+  }
+
+  async presentToast() {
+    const toast = await this._toastCtrl.create({
+      message: 'El producto se ha insertado correctamente.',
+      duration: 1000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 }
---
+++ myFirstApp/src/app/interfaces.ts
@@ -0,0 +1,24 @@
+export interface IProducto{
+    "id" : number,
+    "nombre" : string,
+    "descripcion" : string,
+    "categoria" : string,
+    "precio" : number
+}
+
+export interface IMotor extends IProducto{
+    "tipo_vehiculo" : string,
+    "Km" : number,
+    "fabricacion": number
+}
+
+export interface IInmobiliaria extends IProducto{
+    "metros_cuadrados": number,
+    "num_banyos": number,
+    "num_habitaciones": number,
+    "localidad": string
+}
+
+export interface ITecnologia extends IProducto{
+    "estado_producto": string
+}
